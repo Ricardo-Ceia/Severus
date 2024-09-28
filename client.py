952a9ea5ab3401e095b07628bb8a4620c87fbf9e
Ricardo-Ceia <A49000@alunos.isel.pt>
2024-09-28 19:48:52 +0100

    send messages between client and server

--- client.py
+++ client.py
@@ -3,13 +3,29 @@ import json
 import sys
 import pygame
 
+#ADD A NEWLINE (ENTER) AT THE END OF "-send" TO SEND THE MESSAGE
+def check_send_command(line):
+    print("This is the line :"+line)
+    if '-send' in line:
+        return True
+    return False
+
+def send_line_to_server(socket,line):
+    text_packet = {
+        'packet_type':'text',
+        'size':len(line),
+        'text':line
+    }
+    socket.sendall(bytes(json.dumps(text_packet),encoding = 'utf-8'))
+
+
 HOST = ''
 PORT = 50008
 
 WIDTH = 600
 HEIGHT = 600
 
-NEXT_CHAR_POSITION_WIDTH_OFFSET = 5
+NEXT_CHAR_POSITION_WIDTH_OFFSET = 10
 NEXT_CHAR_POSITION_HEIGHT_OFFSET = 20
 NEW_BOTTOM_BAR_RENDER_OFFSET = 15
 COLOR_BOTTOM_BAR = (255,255, 220)
@@ -27,38 +43,36 @@ running = True
 current_char_width_position = 0
 current_char_height_position = HEIGHT-15
 new_rect_height_offset = HEIGHT - NEW_BOTTOM_BAR_RENDER_OFFSET
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            print("Severus has been closed!")
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            inputed_char = chr(event.key)
-            char_to_be_rendered = font.render(inputed_char,True,COLOR_LETTERS)
-            screen.blit(char_to_be_rendered, (current_char_width_position, current_char_height_position))
-            pygame.display.update()
-            current_char_width_position += NEXT_CHAR_POSITION_WIDTH_OFFSET
-            if current_char_width_position >= WIDTH:
-                current_char_height_position += NEXT_CHAR_POSITION_HEIGHT_OFFSET
-                current_char_width_position = 0
-            #TODO:REVIEW THIS PART OF THE CODE TO RENDER A RECTANGLE ON TOP OF THE OTHER WITHOUT DELETE PREVIOUS WRITTEN LINES 
-            #if current_char_height_position >= HEIGHT:
-                #new_rect_height_offset-=NEW_BOTTOM_BAR_RENDER_OFFSET
-                #pygame.draw.rect(screen,COLOR_BOTTOM_BAR,pygame.Rect(0,new_rect_height_offset,WIDTH,HEIGHT))
-                #pygame.display.update(pygame.Rect(pygame.Rect(0,new_rect_height_offset,WIDTH,HEIGHT)))
-                #current_char_height_position = HEIGHT-15            
 
-'''    
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))i
-    text = input()
-    text_packet={
-        'packet_type':'text',
-        'size':len(text),
-        'data':text
-    }
-    s.sendall(bytes(json.dumps(text_packet),encoding='utf-8'))
-    data_recevied = json.loads(s.recv(1024).decode('utf-8'))
-print('Received ',data_recevied)
-'''
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    s.connect((HOST,PORT))
+    line = ""
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                print("Severus has been closed!")
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                inputed_char = chr(event.key)
+                char_to_be_rendered = font.render(inputed_char,True,COLOR_LETTERS)
+                screen.blit(char_to_be_rendered, (current_char_width_position, current_char_height_position))
+                pygame.display.update()
+                current_char_width_position += NEXT_CHAR_POSITION_WIDTH_OFFSET
+                if check_send_command(line):
+                    send_line_to_server(s,line)
+                    pygame.draw.rect(screen,COLOR_BOTTOM_BAR,pygame.Rect(0,HEIGHT-15,WIDTH,HEIGHT))
+                    pygame.display.update()
+                    current_char_width_position = 0
+                    line = ""
+                    current_char_height_position = HEIGHT-15
+                if current_char_width_position >= WIDTH:
+                    current_char_height_position += NEXT_CHAR_POSITION_HEIGHT_OFFSET
+                    current_char_width_position = 0
+                #TODO:ADD MULTI-LINE SUPPORT 
+                if current_char_height_position >= HEIGHT:
+                    pygame.quit()
+                    print("Severus has been closed! You have reached the max amount of characters!")
+                    sys.exit()
+
+                line += inputed_char
