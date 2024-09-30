214c7027609d549be1234069450ee70a0f484f17
Ricardo-Ceia <A49000@alunos.isel.pt>
2024-09-30 14:02:42 +0100

    adding multiple client conn support

--- server.py
+++ server.py
@@ -3,6 +3,18 @@ import socket
 import threading 
 import json 
 
+
+def handle_client(conn,addr):
+    print(f"New connection: {addr} connected!")
+    connected = True
+    while connected:
+        data = json.loads(conn.recv(1024).decode('utf-8'))
+        if data['packet_type'] == 'conn_close':
+            connected = False
+            print(f"Closing {addr} connection!")
+    conn.close()
+
+
 HOST = ''
 PORT = 50008
 
@@ -10,19 +22,11 @@ PORT = 50008
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST,PORT))
     s.listen(1)
-    conn,addr = s.accept()
-    with conn:
-        print("Connected by ",addr)
-        Connected = True
-        while Connected:
-            data = json.loads(conn.recv(1024).decode('utf-8'))
-            print(f"Client Data:{data}")
-            print(type(data))
-            if data['packet_type'] == 'conn_close':
-                Connected = False
-        conn.close()
-            #conn.sendall(data)
-    #conn.exit()
 
+    while True:
+        conn,addr = s.accept()
+        thread = threading.Thread(target=handle_client,args=(conn,addr))
+        thread.start()
+        print(f"{threading.active_count()}Active connection ")
 
 
